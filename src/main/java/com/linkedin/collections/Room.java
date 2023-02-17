1f0e9eb580d8b75df46799e05505f6419bb6be4d
Fabio <fabio.lovetro@gmail.com>
2023-02-17 14:40:01 +0100

    overriding boolean equals and int hashCode

--- src/main/java/com/linkedin/collections/Room.java
+++ src/main/java/com/linkedin/collections/Room.java
@@ -1,5 +1,7 @@
 package com.linkedin.collections;
 
+import java.util.Objects;
+
 public class Room {
 
     private String name;
@@ -60,4 +62,16 @@ public class Room {
         return "Room [name=" + name + ", type=" + type + ", capacity=" + capacity + ", rate=" + rate + "]";
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Room)) return false;
+        Room room = (Room) o;
+        return getCapacity() == room.getCapacity() && getName().equals(room.getName()) && getType().equals(room.getType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getType(), getCapacity());
+    }
 }
