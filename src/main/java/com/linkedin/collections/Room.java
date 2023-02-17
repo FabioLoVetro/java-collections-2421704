7e0825c43f13e00855d74f77874a0ca5156113ae
Fabio <fabio.lovetro@gmail.com>
2023-02-17 14:09:10 +0100

    tidied code

--- src/main/java/com/linkedin/collections/Room.java
+++ src/main/java/com/linkedin/collections/Room.java
@@ -2,62 +2,62 @@ package com.linkedin.collections;
 
 public class Room {
 
-	private String name;
+    private String name;
 
-	private String type;
+    private String type;
 
-	private int capacity;
+    private int capacity;
 
-	private double rate;
+    private double rate;
 
-	public Room(String name, String type, int capacity, double rate) {
-		this.name = name;
-		this.type = type;
-		this.capacity = capacity;
-		this.rate = rate;
-	}
+    public Room(String name, String type, int capacity, double rate) {
+        this.name = name;
+        this.type = type;
+        this.capacity = capacity;
+        this.rate = rate;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Room(String name, String type) {
+        super();
+        this.name = name;
+        this.type = type;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public int getCapacity() {
-		return capacity;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public void setCapacity(int capacity) {
-		this.capacity = capacity;
-	}
+    public int getCapacity() {
+        return capacity;
+    }
 
-	public double getRate() {
-		return rate;
-	}
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
-	public void setRate(double rate) {
-		this.rate = rate;
-	}
+    public double getRate() {
+        return rate;
+    }
 
-	public Room(String name, String type) {
-		super();
-		this.name = name;
-		this.type = type;
-	}
-	
-	@Override
-	public String toString() {
-		return "Room [name=" + name + ", type=" + type + ", capacity=" + capacity + ", rate=" + rate + "]";
-	}
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "Room [name=" + name + ", type=" + type + ", capacity=" + capacity + ", rate=" + rate + "]";
+    }
 
 }
